de3221976e81e2719a0f5003cac9de7fb80ac225
pradeep <pradeepkumar2497@gmail.com>
2019-07-27 20:38:54 +0530

    launch

---
+++ precache-manifest.ffe9a36b050d5ae506998f21aeb1ea3e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a8b48d4d6667902e69a511102f7288",
+    "url": "/Dashboard-site/index.html"
+  },
+  {
+    "revision": "2f942bd185a1d7b7cab2",
+    "url": "/Dashboard-site/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b9e37b52f7ee56b550f6",
+    "url": "/Dashboard-site/static/css/main.d87be435.chunk.css"
+  },
+  {
+    "revision": "2f942bd185a1d7b7cab2",
+    "url": "/Dashboard-site/static/js/2.beaae3a3.chunk.js"
+  },
+  {
+    "revision": "b9e37b52f7ee56b550f6",
+    "url": "/Dashboard-site/static/js/main.6e22082f.chunk.js"
+  },
+  {
+    "revision": "33e6162959b262a38398",
+    "url": "/Dashboard-site/static/js/runtime~main.08a95f9a.js"
+  },
+  {
+    "revision": "08efb632143669f82b09b39ce641a4d4",
+    "url": "/Dashboard-site/static/media/BG.08efb632.jpg"
+  },
+  {
+    "revision": "d8042407e5389c1883fb1f13e51aae78",
+    "url": "/Dashboard-site/static/media/CMRLOGI.d8042407.jpg"
+  },
+  {
+    "revision": "1c3cde30691295a50a81d34a49eabe1a",
+    "url": "/Dashboard-site/static/media/background_img1.1c3cde30.jpg"
+  },
+  {
+    "revision": "7519ae0726a4ad7c02e2f8ac93eb925b",
+    "url": "/Dashboard-site/static/media/basket.7519ae07.jpg"
+  },
+  {
+    "revision": "4953f4a25c86ee0ebaece9d17c58f97f",
+    "url": "/Dashboard-site/static/media/bg_1.4953f4a2.jpg"
+  },
+  {
+    "revision": "7a7fa971964b6f0fde027b876d6099dd",
+    "url": "/Dashboard-site/static/media/h11b.7a7fa971.jpg"
+  },
+  {
+    "revision": "b6e81fa2ae9159a6a6d5dfa40d007d2b",
+    "url": "/Dashboard-site/static/media/h12.b6e81fa2.jpg"
+  },
+  {
+    "revision": "4e452f4d98ecfc4e180c19b3e1bd16b8",
+    "url": "/Dashboard-site/static/media/handi6.4e452f4d.jpg"
+  },
+  {
+    "revision": "e333b4339850664acaa9fba4e3fe12ca",
+    "url": "/Dashboard-site/static/media/handicraft2.e333b433.jpg"
+  },
+  {
+    "revision": "9378c5b7c693acb2ae91dbc6a6f5fcd8",
+    "url": "/Dashboard-site/static/media/handicraft4.9378c5b7.jpg"
+  }
+]);
